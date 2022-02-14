11204604bfb4ef5d600d7a151642b7b769cb9db7
Alexey Y <alexey.yashenkov@gmail.com>
2022-02-14 17:31:55 +0300

    Create Config.js

---
+++ Config.js
@@ -0,0 +1,15 @@
+// @ts-check
+
+export default class Config {
+  constructor(data = {}) {
+    this.data = data;
+  }
+
+  getValue(key) {
+    const result = this.data[key];
+    if (result instanceof Object) {
+      return new Config(result);
+    }
+    return result;
+  }
+}
