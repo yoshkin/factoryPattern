823f566fab4f9b219b4fed6e1e4820638908f9fe
Alexey Y <alexey.yashenkov@gmail.com>
2022-02-14 17:33:08 +0300

    Create ConfigFactory.test.js

---
+++ ConfigFactory.test.js
@@ -0,0 +1,36 @@
+// @ts-check
+
+import path from 'path';
+import ConfigFactory from './ConfigFactory.js';
+
+describe('ConfigFactory', () => {
+  const pathToFixtures = path.join('__fixtures__'); //need to be
+
+  it('test yml', () => {
+    const filePath = path.join(pathToFixtures, 'test.yml');
+    const config = ConfigFactory.factory(filePath);
+    const actual = config.getValue('key');
+    expect(actual).toBe('value');
+  });
+
+  it('test yaml', () => {
+    const filePath = path.join(pathToFixtures, 'test2.yaml');
+    const config = ConfigFactory.factory(filePath);
+    const actual = config.getValue('key');
+    expect(actual).toBe('another value');
+  });
+
+  it('test json 1', () => {
+    const filePath = path.join(pathToFixtures, 'test.json');
+    const config = ConfigFactory.factory(filePath);
+    const actual = config.getValue('key');
+    expect(actual).toBe('something else');
+  });
+
+  it('test json 2', () => {
+    const filePath = path.join(pathToFixtures, 'test2.json');
+    const config = ConfigFactory.factory(filePath);
+    const actual = config.getValue('files').getValue('config');
+    expect(actual).toBe('json');
+  });
+});
